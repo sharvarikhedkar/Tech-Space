4b7bd515577e534864107c4d374cdba2642be8cb
sharvarikhedkar <anujakhedkar96@gmail.com>
2021-03-29 11:27:43 -0700

    Feat: Added express and formatting

--- socialapp-functions/functions/index.js
+++ socialapp-functions/functions/index.js
@@ -2,46 +2,52 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const { database } = require("firebase-admin");
 
+const express = require('express');
+const app = express();
+
 //initializing application
 admin.initializeApp();
 
-//func for fetching screams created in firestore or to get documents from firestore
-exports.getScreams = functions.https.onRequest((req, res) => {
-  admin
+//get documents from firestore
+app.get('/screams', (req, res) => {
+    admin
     .firestore()
-    .collection("screams")
-    .get()
-    .then((data) => {
+    .collection("screams").get().then((data) => {
       //populating screams with the data created in firestore
       let screams = [];
       data.forEach((doc) => {
-        screams.push(doc.data());
+        screams.push({
+            screamId: doc.id,
+            body: doc.data().body,
+            userHandle: doc.data().userHandle,
+            createdAt: doc.data().createdAt 
+        });
       });
       return res.json(screams);
     })
     .catch((err) => console.error(err));
+})
+
+//create a document
+app.post('/scream',(req, res) => { 
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: new Date().toISOString()
+  };
+
+  admin
+    .firestore()
+    .collection("screams")
+    .add(newScream)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "something went wrong" });
+      console.err(err);
+    });
 });
 
-//function to create a document
-exports.createScream = functions.https.onRequest((req, res) => { 
-  if(req.method !== 'POST'){
-      return res.status(400).json({ error: 'Method not allowed'});
-  }
-const newScream = {
-  body: req.body.body,
-  userHandle: req.body.userHandle,
-  createdAt: admin.firestore.Timestamp.fromDate(new Date()),
-};
-
-admin
-  .firestore()
-  .collection("screams")
-  .add(newScream)
-  .then((doc) => {
-    res.json({ message: `document ${doc.id} created successfully` });
-  })
-  .catch((err) => {
-    res.status(500).json({ error: "something went wrong" });
-    console.err(err);
-  });
-});
+//handle all app routes
+exports.api = functions.https.onRequest(app); 
