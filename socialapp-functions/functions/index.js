3075ae6a354bffbf4982323810182949fd2fa2d0
sharvarikhedkar <anujakhedkar96@gmail.com>
2021-04-16 16:58:42 -0700

    Feat: Added Authentication and Signup users

--- socialapp-functions/functions/index.js
+++ socialapp-functions/functions/index.js
@@ -1,12 +1,23 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const app = require('express')();
 const { database } = require("firebase-admin");
-
-const express = require('express');
-const app = express();
-
-//initializing application
 admin.initializeApp();
+
+const config = {
+  apiKey: "AIzaSyBl7WfnmvkYH9wjPYefHvT8aKkD65GZBHg",
+  authDomain: "socialapp-bc6bd.firebaseapp.com",
+  databaseURL: "https://socialapp-bc6bd-default-rtdb.firebaseio.com",
+  projectId: "socialapp-bc6bd",
+  storageBucket: "socialapp-bc6bd.appspot.com",
+  messagingSenderId: "752702007482"
+  // appId: "1:752702007482:web:7dfee6cd04ea047503fa83",
+  // measurementId: "G-TW8E40PZTW"
+};
+
+
+const firebase = require('firebase');
+firebase.initializeApp(config)
 
 //get documents from firestore
 app.get('/screams', (req, res) => {
@@ -46,6 +57,29 @@ app.post('/scream',(req, res) => {
     .catch((err) => {
       res.status(500).json({ error: "something went wrong" });
       console.err(err);
+    });
+});
+
+//Signup route 
+app.post('/signup', (req, res) => {
+   
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
+
+  //TODO Validate data
+
+  //signing up users 
+  firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then(data => {
+      return res.status(201).json({ message: `user ${data.user.uid} signed up successfully`});
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({error: err.code});
     });
 });
 
