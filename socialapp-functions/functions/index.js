43e41fc0d9c9cbc8f6d17e5fcde1f2b8db572783
sharvarikhedkar <anujakhedkar96@gmail.com>
2021-04-16 18:02:09 -0700

    Feat: Added validation for user handle

--- socialapp-functions/functions/index.js
+++ socialapp-functions/functions/index.js
@@ -19,10 +19,11 @@ const config = {
 const firebase = require('firebase');
 firebase.initializeApp(config)
 
+const db = admin.firestore();
+
 //get documents from firestore
 app.get('/screams', (req, res) => {
-    admin
-    .firestore()
+    db
     .collection("screams").get().then((data) => {
       //populating screams with the data created in firestore
       let screams = [];
@@ -47,8 +48,7 @@ app.post('/scream',(req, res) => {
     createdAt: new Date().toISOString()
   };
 
-  admin
-    .firestore()
+  db
     .collection("screams")
     .add(newScream)
     .then((doc) => {
@@ -70,16 +70,32 @@ app.post('/signup', (req, res) => {
     handle: req.body.handle,
   };
 
-  //TODO Validate data
+  //Validate user handle 
+  db.doc(`/users/${newUser.handle}`).get() 
+    .then(doc => {
+      if(doc.exists){
+        return res.status(400).json({ handle: 'this handle is already taken'});
+      } else {
+        return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+      }
 
-  //signing up users 
-  firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
-    .then(data => {
-      return res.status(201).json({ message: `user ${data.user.uid} signed up successfully`});
     })
-    .catch(err => {
+    .then(data => {
+      return data.user.getIdToken();
+    })
+    //return auth token
+    .then(token => {
+      return res.status(201).json({ token });
+    })
+    .catch((err) => {
       console.error(err);
-      return res.status(500).json({error: err.code});
+      if(err.code === 'auth/email-already-in-use'){
+        return res.status(400).json({email: 'Email is already in use'})
+      }
+      else{
+        return res.status(500).json({error: err.code });
+      }
+      
     });
 });
 
