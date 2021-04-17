f89ca8a3f683823e6c1b5edc24dcff7da71bcd4c
sharvarikhedkar <anujakhedkar96@gmail.com>
2021-04-17 16:42:45 -0700

    Feat: Setup Authentication middleware

--- socialapp-functions/functions/index.js
+++ socialapp-functions/functions/index.js
@@ -41,11 +41,49 @@ app.get('/screams', (req, res) => {
     .catch((err) => console.error(err));
 })
 
-//create a document
-app.post('/scream',(req, res) => { 
+//Authentication middleware 
+const FBAuth = (req, res, next) => {
+  let idToken;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+    console.log
+    //extract idToken
+    idToken = req.headers.authorization.split('Bearer ')[1];
+  } else {
+    console.error('No token found')
+    return res.status(403).json({error: 'Unauthorized'});
+  }
+
+  admin.auth().verifyIdToken(idToken)
+    .then(decodedToken => {
+      req.user = decodedToken;
+      console.log("decoded Token is: ",decodedToken);
+      return db.collection('users')
+        .where('userId', '==', req.user.uid)
+        //limits results to just one document 
+        .limit(1)
+        .get();
+    })
+    .then((data) => {
+      //.data() extracts data from this document 
+      req.user.handle = data.docs[0].data().handle; 
+      return next();
+    })
+    .catch(err => {
+      console.error('Error while verifying the token',err);
+      return res.status(403).json(err);
+    })
+}
+
+//create a document, post one scream
+app.post('/scream', FBAuth, (req, res) => { 
+  
+  if (req.body.body.trim() === '') {
+    return res.status(400).json({ body: 'Body must not be empty' });
+  }
+  
   const newScream = {
     body: req.body.body,
-    userHandle: req.body.userHandle,
+    userHandle: req.user.handle,
     createdAt: new Date().toISOString()
   };
 
