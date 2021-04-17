5b4a506d1008ef30e00a12e07331e425cc2a240f
sharvarikhedkar <anujakhedkar96@gmail.com>
2021-04-16 20:50:07 -0700

    Feat: Added validation for signup route

--- socialapp-functions/functions/index.js
+++ socialapp-functions/functions/index.js
@@ -17,6 +17,7 @@ const config = {
 
 
 const firebase = require('firebase');
+const { json } = require("express");
 firebase.initializeApp(config)
 
 const db = admin.firestore();
@@ -60,6 +61,22 @@ app.post('/scream',(req, res) => {
     });
 });
 
+//helper function to check empty string 
+const isEmpty = (string) => {
+  if(string.trim() === '') return true;
+  else return false
+}
+
+//helper function to check valid email 
+const isEmail = (email) => {
+
+  //using regex that matches a pattern of an email 
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (email.match(regEx)) return true;
+  else return false;
+}
+
 //Signup route 
 app.post('/signup', (req, res) => {
    
@@ -70,8 +87,25 @@ app.post('/signup', (req, res) => {
     handle: req.body.handle,
   };
 
+  let errors = {};
+
+  //validate email
+  if (isEmpty(newUser.email)){
+    //set property in errors object
+    errors.email = 'Must not be empty'
+  } else if(!isEmail(newUser.email)){
+    errors.email = 'Must be a valid email address';
+  } 
+
+  //validation for other fields
+  if(isEmpty(newUser.password)) errors.password = 'Must not be empty';
+  if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match';
+  if(isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+
+  if(Object.keys(errors).length > 0) return res.status(400).json(errors); 
+
   //Validate user handle 
-  let token;
+  let token, userId;
   db.doc(`/users/${newUser.handle}`).get() 
     .then(doc => {
       if(doc.exists){
